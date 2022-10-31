812bc491075c2bd7ebfdb5f7fc51a9a294d990f7
Tmoore52 <tmoore5252@yahoo.com>
2022-10-31 17:29:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = function (a, b) {
+    let total = a + b;
+    return total;
+  };
+  
+  const subtract = function (a, b) {
+    let total = a - b;
+    return total;
+  };
+  
+  const divide = function (a,b) {
+    let total = a/b;
+    return total
+    }
+  
+  const multiply = function (a,b) {
+    let total = a*b;
+    return total
+  };
