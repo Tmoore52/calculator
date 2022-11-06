3b3630cd4e30e1229a1d002b449e175d6937bc0c
Tyler Moore <tylermoore5252@gmail.com>
2022-11-06 10:16:50 -0500

    Display, clear, and delete buttons working

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const multiplyButton = document.getElementById("multiply");
 const divideButton = document.getElementById("divide");
 const display = document.getElementById("display");
 const equation = document.getElementById("equation");
-const numBtn  = document.getElementsByClassName("num-btn");
+const numBtn = document.querySelectorAll(".num-btn");
+const deleteBtn = document.getElementById("delete");
+const clearBtn = document.getElementById("clear");
+let displayValue = "";
 
 const add = function (a, b) {
   let total = a + b;
@@ -27,15 +30,37 @@ const multiply = function (a, b) {
 };
 
 function operate(a, b) {
-  addButton.click(add(a, b));
+addButton.addEventListener("click", add(displayValue, b));
   subtractButton.click(subtract(a, b));
   multiplyButton.click(multiply(a, b));
   divideButton.click(divide(a, b));
 }
 
+function updateDisplay(e) {
+  displayValue += e.target.innerText
+  display.innerText = displayValue;
+}
 
-  buttons.addEventListener("click", showDisplay(this))
+function clearDisplay() {
+  displayValue = "";
+  display.innerText="";
+}
 
-  function showDisplay() {
-    display.innerText += this.innerText;
-  }
+function deleteNum() {
+  displayValue = displayValue.slice(0, -1);
+  display.innerText = displayValue
+}
+
+
+numBtn.forEach(btn => btn.addEventListener("click", updateDisplay));
+
+clearBtn.addEventListener("click", clearDisplay);
+
+deleteBtn.addEventListener("click", deleteNum);
+  
+
+function runEvent(e) {
+  e.preventDefault();
+  console.log("EVENT TYPE:" + e.type);
+  console.log(e.target.innerText);
+}
